7c7e4ac229393009655bd3da09e864d7f15ae096
CarolaOrbuch <101721194+CarolaOrbuch@users.noreply.github.com>
2022-05-09 14:28:14 -0300

    ejercicio terminadi?
    esfera y cubo clonado

---
+++ Assets/scrpits/CubeCloner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeCloner : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject prefab;
+    public GameObject clone;
+
+    void Start()
+    {
+        for (int i = 0; i<3; i++)
+        {
+            clone = Instantiate(prefab);
+            clone.transform.Translate(i, 0, 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
